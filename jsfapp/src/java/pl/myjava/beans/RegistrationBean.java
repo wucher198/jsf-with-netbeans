6629a76efb6a25e36f87ff2301f62a907a7a7ae3
anonim <anonim@ASUSK53.dom>
2017-07-17 19:53:31 +0200

    Chapter 4

---
+++ jsfapp/src/java/pl/myjava/beans/RegistrationBean.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.myjava.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author anonim
+ */
+@ManagedBean
+@RequestScoped
+public class RegistrationBean {
+    private String salutation;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String email;
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    /**
+     * Creates a new instance of RegistrationBean
+     */
+    public RegistrationBean() {
+    }
+    
+}
